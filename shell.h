21c74d86969c6f53c678714210986d1dc26db05a
d-brightmind <robiuridwanullah123@gmail.com>
2023-02-23 12:19:42 -0800

    updating shell.h

--- shell.h
+++ shell.h
@@ -146,8 +146,7 @@ typedef struct passinfo
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
- /*
- *
+/**
  * struct builtin - contains a builtin string and related function
  * @type: the builtin command flag
  * @func: the function
